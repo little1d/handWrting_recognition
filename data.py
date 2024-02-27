34173607979d8da585014430940e39fa329cedd7
Zachary_Yang <115958756+little1d@users.noreply.github.com>
2024-02-27 17:36:12 +0800

    feat(data.py)：data update

--- data.py
+++ data.py
@@ -2,10 +2,23 @@ import torch
 import torchvision
 from torch.utils.data import DataLoader
 
+# 设定batch_size 每次传递给网络的数据样本数量
+batch_size_train = 64
+batch_size_test = 1000
+
+# Data Loader object
 train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST('./data',train=True,download=True
-                               ,transform=torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.137,)(0.3081))
+    torchvision.datasets.MNIST('./data/',train=True,download=True,
+                               transform=torchvision.transforms.Compose([
+                                    torchvision.transforms.ToTensor(),
+                                    torchvision.transforms.Normalize((0.1307,),(0.3081,))
         ])),
-    batch_size=
+    batch_size=batch_size_train,shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('./data/',train=False,download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize((0.1307,),(0.3081,))
+                               ])),
+    batch_size = batch_size_test, shuffle=False)
